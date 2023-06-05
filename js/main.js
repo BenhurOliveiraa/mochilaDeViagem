c02aa7163247fcd6bbb5a0624f9e867aa060765b
BenhurOliveiraa <117686902+BenhurOliveiraa@users.noreply.github.com>
2023-06-05 20:23:38 -0300

    Add files via upload
    Refatorando o código javaScript e adicionando funções.

--- js/main.js
+++ js/main.js
@@ -1,33 +1,46 @@
 const form = document.getElementById("novoItem");
+const lista = document.getElementById('lista');
+const itens = JSON.parse(localStorage.getItem("itens")) || [];
+
+
+itens.forEach( (elemento) => {
+    criaElemento(elemento)
+})
+
 
 form.addEventListener("submit", (evento) => {
     evento.preventDefault();
-    console.log(evento)
-    criaElemento((evento.target.elements['nome'].value), (evento.target.elements['quantidade'].value))
     
+    const nome = evento.target.elements['nome'];
+    const quantidade = evento.target.elements['quantidade']
     
+    const itemAtual = {
+        "nome": nome.value,
+        "quantidade": quantidade.value
+    }
+
+    itens.push(itemAtual);
+
+    localStorage.setItem("itens", JSON.stringify(itens));
+  
+    criaElemento(itemAtual)
     
-    
+    nome.value = ""
+    quantidade.value = ""
 })
 
-function criaElemento (nome, quantidade) {
-    console.log(nome);
-    console.log(quantidade);
+function criaElemento (item) {
 
-    //<li class="item"><strong>7</strong>Camisas</li>
-    const novoItem = document.createElement('li');
+    const novoItem = document.createElement("li");
     novoItem.classList.add("item");
 
     const numeroItem = document.createElement("strong");
-    numeroItem.innerHTML = quantidade;
-
+    numeroItem.innerHTML = item.quantidade;
     novoItem.appendChild(numeroItem);
-    novoItem.innerHTML += nome;
 
-    const lista = document.getElementById('lista');
+    novoItem.innerHTML += item.nome;
+
     lista.appendChild(novoItem);
-
-    console.log(novoItem);
 }
 
 
